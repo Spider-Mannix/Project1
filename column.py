79115cef8fdd9501c0f5fc2e11ab079946f1a53b
Dave <dave90mannix@gmail.com>
2019-04-23 19:28:14 +0100

    var for each attribute

--- column.py
+++ column.py
@@ -1,9 +1,28 @@
+#from https://stackoverflow.com/questions/30216573/reading-specific-columns-from-a-text-file-in-python
+
+#open and read data file
 f=open("irisdata.txt","r")
 lines=f.readlines()
-result=[]
+
+#variables for each attribute column: sepal length & width, petal length & width, and iris class
+seplen=[]
+sepwit=[]
+petlen=[]
+petwit=[]
+cliris=[]
+
 for x in lines:
-    result.append(x.split(',')[0])
-  
+    #use append to fill in blank lists
+    seplen.append(x.split(',')[0])
+    sepwit.append(x.split(',')[1])
+    petlen.append(x.split(',')[2])
+    petwit.append(x.split(',')[3])
+    cliris.append(x.split(',')[4])
+
+#close file (good practice)  
 f.close()
 
-print(result)  
+print(cliris)  
+print(len(cliris))
+
+#code can now be used in other python files
