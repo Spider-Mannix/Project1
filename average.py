b7a5447c5aa769b7b94e5e6e0b716b477ca04cc2
Dave <dave90mannix@gmail.com>
2019-04-24 22:15:03 +0100

    trying to use numpy.mean

---
+++ average.py
@@ -0,0 +1,28 @@
+#for averages use numpy/scipy
+#https://stackoverflow.com/questions/7716331/calculating-arithmetic-mean-one-type-of-average-in-python
+#https://docs.scipy.org/doc/numpy/reference/generated/numpy.mean.html
+#check numpy stats library for useful information
+
+import numpy
+
+f=open("irisdata.txt","r")
+lines=f.readlines()
+
+seplen=[]
+sepwit=[]
+petlen=[]
+petwit=[]
+cliris=[]
+
+for x in lines:
+    seplen.append(x.split(',')[0])
+    sepwit.append(x.split(',')[1])
+    petlen.append(x.split(',')[2])
+    petwit.append(x.split(',')[3])
+    cliris.append(x.split(',')[4].rstrip('\n')) 
+
+f.close()
+
+slmean = numpy.mean(seplen)
+
+print(slmean)
