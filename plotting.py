3503bbe6019ac4bdd7b16d5a701292a3ab3b1119
Dave <dave90mannix@gmail.com>
2019-05-01 22:35:52 +0100

    trying numpy.histogram

--- plotting.py
+++ plotting.py
@@ -1,5 +1,6 @@
 #from Problem 10 
 import matplotlib.pyplot as pl
+import numpy
 
 f=open("irisdata.txt","r")
 lines=f.readlines()
@@ -12,6 +13,7 @@ cliris=[]
 
 for x in lines:
     seplen.append(x.split(',')[0])
+    seplen = [float(i) for i in seplen]
     sepwit.append(x.split(',')[1])
     petlen.append(x.split(',')[2])
     petwit.append(x.split(',')[3])
@@ -23,5 +25,7 @@ f.close()
 #seplen.sort()
 #pl.plot(seplen)
 sepwit.sort()
-pl.plot(sepwit)
+#pl.plot(sepwit)
+slhist = numpy.histogram(seplen)
+pl.plot(slhist)
 pl.show()
