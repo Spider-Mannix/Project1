212573e9127d2a71c6320bb660634c17ec7ad532
Dave <dave90mannix@gmail.com>
2019-05-05 08:30:13 +0100

    converted all to floats

--- column.py
+++ column.py
@@ -14,22 +14,26 @@ cliris=[]
 for x in lines:
     #use append to fill in blank lists
     seplen.append(x.split(',')[0])
+    seplen = [float(i) for i in seplen] #make floating point numbers
     #separate by class
     slset = seplen[0:50]
     slvers = seplen[50:100]
     slgin = seplen[100:150]
 
     sepwit.append(x.split(',')[1])
+    sepwit = [float(i) for i in sepwit]
     swset = sepwit[0:50]
     swvers = sepwit[50:100]
     swgin = sepwit[100:150]
 
     petlen.append(x.split(',')[2])
+    petlen = [float(i) for i in petlen]
     plset = petlen[0:50]
     plvers = petlen[50:100]
     plgin = petlen[100:150]
 
     petwit.append(x.split(',')[3])
+    petwit = [float(i) for i in petwit]
     pwset = petwit[0:50]
     pwvers = petwit[50:100]
     pwgin = petwit[100:150]
